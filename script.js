c309ed83f86448b88125d4c8d86de9625cd3cf06
Jannatul Ferdous Munni <107238449+Aliajannat@users.noreply.github.com>
2023-08-04 02:52:47 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// write your JavaScript here
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+}
